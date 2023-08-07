0a883b9c5c1cc462de86c69dc8c35d1a6cc25845
Natan <dr.natanael.lima@gmail.com>
2023-08-07 13:55:12 -0300

    Add toolbar

--- src/components/Layout/Topbar.jsx
+++ src/components/Layout/Topbar.jsx
@@ -1,6 +1,5 @@
-import { AppBar, Toolbar, makeStyles } from "@mui/material";
-import { Box } from "@mui/material/Box";
-import { Icons } from "@mui/material/Icons";
+import { AppBar, Box, Button, Toolbar, makeStyles } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -8,16 +7,34 @@ const useStyles = makeStyles((theme) => ({
     zIndex: theme.zIndex.drawer + 1,
     backgroundColor: theme.palette.background.default,
   },
+  toolbar: {
+    minHeight: 56,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  logo: {
+    cursor: "pointer",
+    height: 18,
+    marginLeft: theme.spacing(3),
+  },
 }));
 export default function Topbar() {
-  const classes = useStyle();
+  const classes = useStyles();
   return (
-    <AppBar className={classes.root} color="default">
-      <Toolbar>
-        <Box></Box>
-        <Box></Box>
-        <Box></Box>
-      </Toolbar>
-    </AppBar>
+     <AppBar className={classes.root} color="default">
+       <Toolbar className={classes.toolbar}>
+        <Box display="flex" alignItems="center">
+         <MenuIcon />
+         <img src="logo-youtube.png" alt="logo" className={classes.logo} />
+         </Box>
+         <Box></Box>
+         <Box>
+          <button>
+
+          </button>
+         </Box>
+       </Toolbar>
+     </AppBar>
   );
 }
--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,6 +1,8 @@
 import { makeStyles } from "@material-ui/styles";
 import Head from "next/head";
 
+import Topbar from "./Topbar";
+
 const useStyles = makeStyles((theme) => ({
   root: {
     backgroundColour: theme.palette.background.dark,
@@ -45,7 +47,7 @@ export default function Layout({ children, title }) {
         <title>{title}</title>
       </Head>
       <div className={classes.root}>
-        <div>topbar</div>
+        <Topbar></Topbar>
         <div>navbar</div>
         <div className={classes.weapper}>
           <div className={classes.contenContainer}>
